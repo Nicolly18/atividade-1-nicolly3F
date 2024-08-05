e34ff9eb292b561ec1311296b5cbc38b1d8992f9
Nicolly18 <135607022+Nicolly18@users.noreply.github.com>
2024-08-05 22:53:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const meuNome = "nicolly linda";
+const idade = 17;
+const altura = 1.58;
+const peso = 48;
+const cidade = "São Jose dos Pinhais"
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(peso);
+console.log(cidade);
